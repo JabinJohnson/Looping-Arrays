81c51908c00ec3b34fe25aad0cc5379ee58156fd
JabinJohnson <133173143+JabinJohnson@users.noreply.github.com>
2023-06-18 16:10:12 -0400

    Add files via upload

---
+++ Looping & Arrays.js
@@ -0,0 +1,16 @@
+for(let i = 0; i => 1000; i -= 2) {
+    console.log(i);
+}
+for(let i = 0; i <= 10000; i++) {
+    if(i === 2500) {
+        alert="A quarter of the way there!";
+    } 
+    if(i === 5000){
+        alert="Halfway there!";
+    }
+    if(i === 10000)
+    alert="The loop is done!";
+}
+const favTVshows=["Sonic Prime","Miraculous Ladybug","Glitch Techs","Code Lyoko","Teen Titans(Original)"];{
+    console.log("My #" + Array.length `favorite tv show is ${favTVshows}`);
+}
